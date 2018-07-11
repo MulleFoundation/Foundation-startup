c219130b82ec549cad54b7068b12dffeb2bfb747
Nat! <nat@mulle-kybernetik.com>
2018-07-11 14:36:55 +0200

    add mulle-project

---
+++ src/Foundation-standalone.m
@@ -0,0 +1,2 @@
+/* the required "anything linkable symbol" to get a shared libary going */
+int   __foundation_unused__ = 1848;
---
+++ src/Foundation.m
@@ -0,0 +1,12 @@
+#include "include-private.h"
+
+#include "Foundation.h"
+
+
+int   __FOUNDATION_ranlib__;
+
+
+uint32_t   Foundation_get_version( void)
+{
+   return( FOUNDATION_VERSION);
+}
